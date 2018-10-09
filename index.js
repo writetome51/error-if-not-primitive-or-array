88c90df0e110bcaf1b900d772cc50677debd0d96
Steve Thompson <writetome51@gmail.com>
2018-10-09 01:38:23 -0600

    added index.js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var isArray_notArray_1 = require("basic-data-handling/isArray_notArray");
+
 function errorIfNotPrimitiveOrArray(value) {
-    var primitives = ['number', 'string', 'boolean'];
-    if (!(primitives.includes(typeof value)) && isArray_notArray_1.notArray(value)) {
-        throw new Error('Input must be either a primitive type or an array');
-    }
+	var primitives = ['number', 'string', 'boolean'];
+	if (!(primitives.includes(typeof value)) && isArray_notArray_1.notArray(value)) {
+		throw new Error('Input must be either a primitive type or an array');
+	}
 }
+
 exports.errorIfNotPrimitiveOrArray = errorIfNotPrimitiveOrArray;
